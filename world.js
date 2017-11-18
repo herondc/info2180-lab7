9963cba5ff986896100e0ac5633f277f46b6efba
herondc <32229486+herondc@users.noreply.github.com>
2017-11-17 23:55:55 -0500

    first commit lab 7

---
+++ world.js
@@ -0,0 +1,95 @@
+window.onload=function(){
+    
+    var btn= document.getElementById('lookup');
+    var httpRequest;
+    httpRequest = new XMLHttpRequest();
+    
+    btn.addEventListener('click',function(element) {
+        element.preventDefault();
+        
+        var checked=document.getElementById('rundllshell').checked;
+        if (checked==true) {
+            
+        
+      
+       
+         httpRequest.onreadystatechange = processAll;
+         httpRequest.open('GET','world.php?country=all=true', true);
+         httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+         httpRequest.send();
+         
+         
+            
+        } else {
+            
+            
+            
+         var country_search=document.getElementById('country').value;
+        
+         
+    
+       
+         httpRequest.onreadystatechange = processCountry;
+         httpRequest.open('GET','world.php?country='+country_search, true);
+         httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+         httpRequest.send();
+         
+       
+            
+        }
+       
+    
+    });
+    
+    
+    
+    function processAll() {
+             
+              if(httpRequest.readyState===XMLHttpRequest.DONE){
+                 if(httpRequest.status===200){
+            
+                    var response=httpRequest.responseText;
+                    var output=document.getElementById('result');
+            
+                    output.innerHTML="<h1> Search Result<h1/><br>"+response;
+                    
+                     
+                }
+                else{
+                 
+                 alert("There was a problem with the request");
+                 
+                    
+                }
+             }
+             
+         }
+    
+    
+    
+    
+    
+    
+      function processCountry() {
+             if(httpRequest.readyState===XMLHttpRequest.DONE){
+                 if(httpRequest.status===200){
+            
+                    var response=httpRequest.responseText;
+                    var output=document.getElementById('result');
+            
+                    output.innerHTML="<h1> Search Result<h1/><br>"+response;
+                    
+                     
+                }
+                else{
+                 
+                 alert("There was a problem with the request");
+                 
+                    
+                }
+             }
+             
+         }
+    
+
+};
---
+++ world.php
@@ -0,0 +1,46 @@
+<?php
+$host = getenv('IP');
+$username = getenv('C9_USER');
+$password = '';
+$dbname = 'world';
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+    
+    $country=$_GET['country'];
+    
+    
+    if ($country=='all=true') {
+        
+    $stmt = $conn->prepare("SELECT * FROM countries ;");
+     
+     $stmt->execute();
+      
+     $results=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo '<ul>';
+   foreach ($results as $row) {
+    echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+   }
+   echo '</ul>';
+        
+    } else {
+        
+        
+      $stmt = $conn->prepare("SELECT * FROM countries WHERE name='$country';");
+     
+      $stmt->execute();
+      
+      $results=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) {
+    echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
+    }
